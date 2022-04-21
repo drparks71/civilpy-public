e0acd4a50025f50a95015866fa8a2abb0a87ca72
Dane <drparks71@gmail.com>
2022-04-21 14:55:46 -0400

    Transfer files from private repo

---
+++ civilpy/structural/search_tools.py
@@ -0,0 +1,79 @@
+fhwa_43a = {
+    '0': 'Other',
+    '1': 'Concrete',
+    '2': 'Concrete (Continuous)',
+    '3': 'Steel',
+    '4': 'Steel (Continuous)',
+    '5': 'Prestressed/Post-Tensioned Concrete',
+    '6': 'Prestressed/Post-Tensioned Concrete (Continuous)',
+    '7': 'Wood or Timber',
+    '8': 'Masonry',
+    '9': 'Cast Iron, Aluminum, or Wrought Iron',
+}
+
+fhwa_43b = {
+    '01': 'Slab',
+    '02': 'Stringer/Multi-beam or Girder',
+    '03': 'Girder and Floorbeam System',
+    '04': 'Tee Beam',
+    '05': 'Box Beam or Girders - Multiple',
+    '06': 'Box Beam or Girders - Single or Spread',
+    '07': 'Frame (except frame culverts)',
+    '08': 'Orthotropic',
+    '09': 'Truss - Deck',
+    '10': 'Truss - Through',
+    '11': 'Arch - Deck',
+    '12': 'Arch - Through',
+    '13': 'Suspension',
+    '14': 'Stayed Girder',
+    '15': 'Movable - Lift',
+    '16': 'Movable - Bascule',
+    '17': 'Movable - Swing',
+    '18': 'Tunnel',
+    '19': 'Culvert (includes frame culverts)',
+    '20': 'Mixed Types (can only be used in approach spans [item 44])',
+    '21': 'Segmental Box Girder',
+    '22': 'Channel Beam',
+    '00': 'Other',
+}
+
+ohio_43c = {
+    '1': 'Pony Truss',
+    '2': 'Through Girder',
+    '3': 'Filled Arch',
+    'N': 'Not Applicable'
+}
+
+ohio_107 = {
+    '1': 'Jack Arch',
+    '2': 'Slab prestressed in both directions',
+    '3': 'Slab prestressed longitudinally only',
+    '4': 'Slab prestressed transverse only',
+    '5': 'Prestressed slab on beams'
+}
+
+ohio_108 = {
+    '1': 'Super Plasticized',
+    '2': 'MicroSilica',
+    '3': 'Chip and Seal',
+    '4': 'Polyester',
+    'N': 'Not Applicable'
+}
+
+ohio_482 = {
+    '1': 'Red lead',
+    '2': 'Unpainted Weathered Steel',
+    '3': 'Paint System A',
+    '4': 'Paint System B',
+    '5': 'Paint System OZEU',
+    '6': 'Galvanized',
+    '7': 'Metalized (Alum/Zinc)',
+    '8': 'Paint System A with intermediate tie coat',
+    '9': 'Paint System IZEU',
+    '0': 'Other Paint',
+    'A': 'EEU',
+    'B': 'Epoxy – Urethane sealers',
+    'C': 'Non-Epoxy sealers',
+    'N': 'None or Not Applicable',
+    'U': 'Unknown Sealant',
+}
